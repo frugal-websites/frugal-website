4acb4b83c7c9edf4308db0332367676bcb53fa39
JeHuppe <jeremie.huppe@gmail.com>
2020-12-20 15:37:48 -0500

    Update data from form

--- src/components/app/Profile/Profile.tsx
+++ src/components/app/Profile/Profile.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react"
-import WebsiteLayout from "../../website/WebsiteLayout/WebsiteLayout"
+import React, { useContext } from "react"
 import { WebsiteEmailIdContext } from "../WebsiteEmailIdProvider/WebsiteEmailIdProvider"
 import { RouteComponentProps } from "@reach/router"
 import {
@@ -11,8 +10,7 @@ import {
 } from "@material-ui/core"
 import AppLayout from "../AppLayout/AppLayout"
 import { useForm, Controller } from "react-hook-form"
-import Input from "@material-ui/core/Input"
-import { gql, useQuery } from "@apollo/client"
+import { gql, useQuery, useMutation } from "@apollo/client"
 
 interface IFormInput {
   websiteEmailId: string
@@ -30,12 +28,25 @@ const defaultValues: IFormInput = {
   appLanguage: "fr",
 }
 
-const GET_FORM_DATA = gql`
-  query {
-    layout {
-      title
-      description
-      isSecondDescription
+const GET_WEBSITE_DATA = gql`
+  query($websiteEmailId: String!) {
+    website(query: { websiteEmailId: $websiteEmailId }) {
+      websiteEmailId
+      notificationEmail
+      firstName
+      phoneNumber
+      appLanguage
+    }
+  }
+`
+
+const UPDATE_WEBSITE_DATA = gql`
+  mutation($websiteEmailId: String!, $formData: WebsiteUpdateInput!) {
+    updateOneWebsite(
+      query: { websiteEmailId: $websiteEmailId }
+      set: $formData
+    ) {
+      websiteEmailId
     }
   }
 `
@@ -43,113 +54,106 @@ const GET_FORM_DATA = gql`
 interface IProfileProps extends RouteComponentProps {}
 
 const Profile: React.FunctionComponent<IProfileProps> = ({}: IProfileProps) => {
-  const { handleSubmit, register, reset, control } = useForm<IFormInput>({
+  const websiteEmailId = useContext(WebsiteEmailIdContext)
+
+  const { handleSubmit, reset, control } = useForm<IFormInput>({
     defaultValues,
   })
 
-  const { loading, error, data } = useQuery(GET_FORM_DATA, {
+  const { loading, error } = useQuery(GET_WEBSITE_DATA, {
+    variables: { websiteEmailId },
     onCompleted: data => {
-      console.log("DATAAAAAA", data)
-      reset({
-        websiteEmailId: "aaaa",
-        firstName: "a21qr",
-        phoneNumber: "aaa",
-        appLanguage: "fr",
-      })
+      reset(data.website)
     },
   })
 
+  const [
+    updateWebsiteData,
+    { loading: mutationLoading, error: mutationError },
+  ] = useMutation(UPDATE_WEBSITE_DATA)
+
   const onSubmit = (formData: IFormInput) => {
-    alert(JSON.stringify(formData))
+    console.log(formData)
+    updateWebsiteData({
+      variables: { websiteEmailId, formData },
+    })
   }
 
   if (loading) return <p>LOADING</p>
   if (error) return <p>ERROR</p>
 
   return (
-    <WebsiteEmailIdContext.Consumer>
-      {websiteEmailId => (
-        <AppLayout>
-          <div>
-            <h2>Welcome back to your profile, {websiteEmailId}!</h2>
-            <h4>TODO add info about: </h4>
-            <ul>
-              <li>FirstName</li>
-              <li>Fr or Eng for app</li>
-              <li>Phone Number</li>
-              <li>FR or En for website.</li>
-              <li>Payments details</li>
-            </ul>
-          </div>
+    <AppLayout>
+      <div>
+        <h2>Welcome back to your profile, {websiteEmailId}!</h2>
+        <h4>TODO add info about: </h4>
+        <ul>
+          <li>FirstName</li>
+          <li>Fr or Eng for app</li>
+          <li>Phone Number</li>
+          <li>FR or En for website.</li>
+          <li>Payments details</li>
+        </ul>
+      </div>
 
-          <Box>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <Box className="container">
-                <section>
-                  <label>Account Email</label>
-                  <Controller
-                    as={TextField}
-                    name="websiteEmailId"
-                    control={control}
-                  />
-                </section>
-                <section>
-                  <label>Notification Email</label>
-                  <Controller
-                    as={TextField}
-                    name="notificationEmail"
-                    control={control}
-                  />
-                </section>
+      <Box>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Box className="container">
+            <section>
+              <label>Account Email</label>
+              <Controller
+                as={TextField}
+                name="websiteEmailId"
+                control={control}
+              />
+            </section>
+            <section>
+              <label>Notification Email</label>
+              <Controller
+                as={TextField}
+                name="notificationEmail"
+                control={control}
+              />
+            </section>
 
-                <section>
-                  <label>First Name</label>
-                  <Controller
-                    as={TextField}
-                    name="firstName"
-                    control={control}
-                  />
-                </section>
+            <section>
+              <label>First Name</label>
+              <Controller as={TextField} name="firstName" control={control} />
+            </section>
 
-                <section>
-                  <label>Phone Number</label>
-                  <Controller
-                    as={TextField}
-                    name="phoneNumber"
-                    control={control}
-                  />
-                </section>
+            <section>
+              <label>Phone Number</label>
+              <Controller as={TextField} name="phoneNumber" control={control} />
+            </section>
 
-                <section>
-                  <label>App language</label>
-                  <Controller
-                    name="appLanguage"
-                    defaultValue=""
-                    control={control}
-                    as={
-                      <RadioGroup aria-label="language">
-                        <FormControlLabel
-                          value="fr"
-                          control={<Radio />}
-                          label="French"
-                        />
-                        <FormControlLabel
-                          value="en"
-                          control={<Radio />}
-                          label="English"
-                        />
-                      </RadioGroup>
-                    }
-                  />
-                </section>
-              </Box>
-
-              <button className="button">submit</button>
-            </form>
+            <section>
+              <label>App language</label>
+              <Controller
+                name="appLanguage"
+                defaultValue=""
+                control={control}
+                as={
+                  <RadioGroup aria-label="language">
+                    <FormControlLabel
+                      value="fr"
+                      control={<Radio />}
+                      label="French"
+                    />
+                    <FormControlLabel
+                      value="en"
+                      control={<Radio />}
+                      label="English"
+                    />
+                  </RadioGroup>
+                }
+              />
+            </section>
           </Box>
-        </AppLayout>
-      )}
-    </WebsiteEmailIdContext.Consumer>
+
+          <button className="button">submit</button>
+        </form>
+      </Box>
+    </AppLayout>
   )
 }
 
